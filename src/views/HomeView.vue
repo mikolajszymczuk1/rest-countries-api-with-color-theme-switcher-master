b24ddd8ef1b45e625d8e0ac20619048ac2b13419
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-07-08 00:44:19 +0200

    Added all needed components (only mobile version)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,31 @@
 <template>
-  <HelloWorld />
+  <div>
+    <!-- ==== Home View ==== -->
+
+    <!-- === Header === -->
+    <header>
+      <NavigationBar />
+    </header>
+
+    <!-- === Main === -->
+    <main>
+      <!-- Section with all app search/filter tools -->
+      <section class="mx-[16px] mt-[24px]">
+        <SearchBar
+          class="mb-[40px]"
+          @searched-country="handleSearchedCountry"
+        />
+        <RegionFilter @region-filter="handleRegionFilter" />
+      </section>
+    </main>
+  </div>
 </template>
 
 <script setup>
-import HelloWorld from '@/components/HelloWorld.vue';
+import NavigationBar from '@/components/NavigationBar.vue';
+import SearchBar from '@/components/SearchBar.vue';
+import RegionFilter from '@/components/RegionFilter.vue';
+
+const handleSearchedCountry = (searchValue) => console.log(searchValue);
+const handleRegionFilter = (region) => console.log(region);
 </script>
