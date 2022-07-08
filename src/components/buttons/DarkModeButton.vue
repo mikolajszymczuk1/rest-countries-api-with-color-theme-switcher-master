f7e80a62ae64a1524b3bb142e4754af29363956a
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-07-08 22:16:10 +0200

    Setup dark mode toggle system (Tailwind manual dark mode)

--- src/components/buttons/DarkModeButton.vue
+++ src/components/buttons/DarkModeButton.vue
@@ -3,6 +3,7 @@
   <button
     class="flex justify-center items-center text-[0.75rem] font-semibold text-very-dark-blue-b
     sm:text-[0.85rem] lg:text-[1rem]"
+    @click="toggleDarkMode()"
   >
     <MoonIcon class="w-[16px] h-[16px] mr-[8px] lg:w-[20px] lg:h-[20px]" />
     Dark Mode
@@ -10,5 +11,38 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
 import MoonIcon from '@/components/icons/MoonIcon.vue';
+
+/* Dark mode active status */
+const isDarkModeActive = ref(false);
+
+/* Based on idDarkModeActive set dark mode class on root html element */
+const setRootDarkModeClass = () => {
+  if (isDarkModeActive.value) {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+  }
+};
+
+/* Update current dark mode status */
+const updateDarkModeStatus = () => {
+  if (!localStorage.getItem('darkMode')) {
+    localStorage.setItem('darkMode', false);
+  }
+
+  isDarkModeActive.value = localStorage.getItem('darkMode');
+  setRootDarkModeClass();
+};
+
+/* Change dark mode active status and update dark mode status in local storage */
+const toggleDarkMode = () => {
+  isDarkModeActive.value = !isDarkModeActive.value;
+  localStorage.setItem('darkMode', isDarkModeActive.value);
+  setRootDarkModeClass();
+};
+
+/* Update dark mode class when component is created */
+onMounted(() => updateDarkModeStatus());
 </script>
