61175d4bed91a9553304724d4ba3e161581ca072
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-07-10 13:46:29 +0200

    Remove unused things

--- src/components/HelloWorld.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1 class="text-[2em] text-center pt-4 text-dark-blue font-nunito font-semibold">
-    {{ msg }}
-  </h1>
-</template>
-
-<script setup>
-const msg = 'Hello World !!!';
-</script>
