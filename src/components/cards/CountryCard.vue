142ef4de36aecfb9782d330e9fc3e491251723b4
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-07-15 01:20:56 +0200

    Created country card component (CountryCard.vue)

--- src/components/cards/CountryCard.vue
+++ src/components/cards/CountryCard.vue
@@ -1,7 +1,48 @@
 <template>
-  <h3>Country Card !</h3>
+  <!-- Country card component -->
+  <div class="w-[264px] rounded-[5px] overflow-hidden shadow-[0_0_7px_2px_rgba(0,0,0,0.03)]">
+    <!-- Card country flag -->
+    <div
+      class="w-[100%] h-[160px] bg-no-repeat bg-cover bg-center"
+      :style="`background-image: url('${cardData.flag}')`"
+    />
+
+    <!-- Card basic information -->
+    <div class="font-nunito px-[24px] pt-[24px] pb-[46px]">
+      <h2 class="font-extrabold text-very-dark-blue-b text-[1.125rem] mb-[16px]">
+        {{ cardData.name }}
+      </h2>
+
+      <p class="text-[0.875rem]">
+        <span class="font-semibold">Population:</span>
+        {{ cardData.population }}
+      </p>
+
+      <p class="text-[0.875rem]">
+        <span class="font-semibold">Region:</span>
+        {{ cardData.region }}
+      </p>
+
+      <p class="text-[0.875rem]">
+        <span class="font-semibold">Capital:</span>
+        {{ cardData.capital }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script setup>
-
+defineProps({
+  cardData: {
+    type: Object,
+    required: true,
+    validator(value) {
+      return 'name' in value
+          && 'population' in value
+          && 'region' in value
+          && 'capital' in value
+          && 'flag' in value;
+    },
+  },
+});
 </script>
