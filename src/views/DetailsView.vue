f4e7dcac2e344d507641b2b790759fe2c12ac720
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-07-24 02:40:41 +0200

    Added data-test attributes

--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -16,6 +16,7 @@
           class="flex justify-center items-center shadow-[0_0_7px_0_rgba(0,0,0,0.3)]
           py-[6px] w-[104px] dark:text-white dark:bg-dark-blue rounded-[2px]
           lg:rounded-[10px] lg:py-[10px] lg:w-[136px] dark:shadow-none"
+          data-test="back-homepage-button"
         >
           <ArrowIcon class="mr-[8px] lg:w-[20px] lg:h-[20px]" />
           Back
@@ -41,44 +42,63 @@
           <h1
             class="font-nunito font-extrabold text-[1.375rem] mb-[16px] sm:mb-[23px]
             lg:text-[2rem]"
+            data-test="country-details-name"
           >
             {{ countryDetails.name }}
           </h1>
 
           <div class="mb-[34px] font-nunito text-[0.875rem] sm:flex lg:text-[1rem] lg:mb-[68px]">
             <div class="mb-[32px] sm:mb-0 sm:mr-[117px] lg:mr-[141px]">
-              <p class="mb-[10px]">
+              <p
+                class="mb-[10px]"
+                data-test="country-details-native-name"
+              >
                 <span class="font-semibold">Native Name:</span>
                 {{ countryDetails.nativeName }}
               </p>
 
-              <p class="mb-[10px]">
+              <p
+                class="mb-[10px]"
+                data-test="country-details-population"
+              >
                 <span class="font-semibold">Population:</span>
                 {{ countryDetails.population.toLocaleString() }}
               </p>
 
-              <p class="mb-[10px]">
+              <p
+                class="mb-[10px]"
+                data-test="country-details-region"
+              >
                 <span class="font-semibold">Region:</span> {{ countryDetails.region }}
               </p>
 
-              <p class="mb-[10px]">
+              <p
+                class="mb-[10px]"
+                data-test="country-details-sub-region"
+              >
                 <span class="font-semibold">Sub Region:</span>
                 {{ countryDetails.subRegion }}
               </p>
 
-              <p>
+              <p data-test="country-details-capital">
                 <span class="font-semibold">Capital:</span>
                 {{ countryDetails.capital }}
               </p>
             </div>
 
             <div>
-              <p class="mb-[10px]">
+              <p
+                class="mb-[10px]"
+                data-test="country-details-top-level-domain"
+              >
                 <span class="font-semibold">Top Level Domain:</span>
                 {{ countryDetails.topLevelDomain }}
               </p>
 
-              <p class="mb-[10px]">
+              <p
+                class="mb-[10px]"
+                data-test="country-details-currencies"
+              >
                 <span class="font-semibold">Currencies:</span>
                 {{ countryDetails.currencies }}
               </p>
@@ -88,6 +108,7 @@
                 <span
                   v-for="lang in countryDetails.languages"
                   :key="lang"
+                  data-test="country-details-language"
                 >
                   {{ lang }},
                 </span>
@@ -103,6 +124,7 @@
                 :key="borderCountry"
                 class="font-nunito text-[0.75rem] mr-[10px] px-[15px] py-[6px]
                 shadow-[0_0_4px_1px_rgba(0,0,0,0.11)] lg:text-[0.875rem] dark:bg-dark-blue"
+                data-test="country-details-border-country"
               >
                 {{ borderCountry }}
               </div>
